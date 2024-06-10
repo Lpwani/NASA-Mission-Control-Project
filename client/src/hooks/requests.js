378b796767348e3577403ba4a421004bde303968
Lokesh Wani <lokeshwani7303@gmail.com>
2024-06-10 18:39:57 +0530

    initial parent commit

---
+++ client/src/hooks/requests.js
@@ -0,0 +1,32 @@
+const API_URL = 'v1';
+
+// Load planets and return as JSON.
+async function httpGetPlanets() {
+    // TODO: Once Api is ready
+    // Load planets and return as JSON
+}
+
+// Load launches, sort by flight number, and return as JSON.
+async function httpGetLaunches() {
+    // TODO: Once Api is ready
+    // Load launches, sort by flight number, and return as JSON.
+}
+
+// Submit given launch data to launch system.
+async function httpSubmitLaunch(launch) {
+    // TODO: Once Api is ready
+    // Submit given launch data to launch system.
+}
+
+// Delete launch with given ID.
+async function httpAbortLaunch(id) {
+    // TODO: Once Api is ready
+    // Delete launch with given ID
+}
+
+export {
+  httpGetPlanets,
+  httpGetLaunches,
+  httpSubmitLaunch,
+  httpAbortLaunch,
+};
---
+++ server/src/server.js
@@ -0,0 +1,6 @@
+import http from 'http';
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
